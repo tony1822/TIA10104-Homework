b70fbd4885004e93dcc04758ce8bab9a3f55bbff
Tony Hsieh <kt505151522@gmail.com>
2024-02-03 16:47:28 +0800

    addhomework

--- src/HW2/Hw2.java
+++ src/HW2/Hw2.java
@@ -57,23 +57,12 @@ public class Hw2 {
 
 		
 		// 請設計一隻Java程式,輸出結果為以下:
-		for (int i = 11; i >= 1; i--) {
-			for (int j = 1; j < i; j++) {
-				System.out.print(j);
-			}System.out.println();
-		}
+		
 		
 		System.out.println("=============================================================================");
 		// 請設計一隻Java程式,輸出結果為以下:
 			
-		int u =65;
-		for(int i=1;i<=6;i++) {
-			for(int j=0;j<i;j++) {				
-				System.out.print((char)(u));				
-			}System.out.println();	u++;
-		}
 		
-		System.out.println("========================================================");
 		
 		
 		// 使用for+while輸出九九乘法算數
