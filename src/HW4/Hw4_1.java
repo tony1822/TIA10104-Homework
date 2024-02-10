23dbc38752c0335f05249ecd956a555490308b2f
Tony Hsieh <kt505151522@gmail.com>
2024-02-10 15:50:28 +0800

    update hw4

--- src/HW4/Hw4_1.java
+++ src/HW4/Hw4_1.java
@@ -58,50 +58,51 @@ public class Hw4_1 {
 		// 請設計一個程式,可以讓小華輸入欲借的金額後,便會顯示哪些員工編號的同事
 		// 有錢可借他;並且統計有錢可借的總人數:例如輸入 1000 就顯示「有錢可借的
 		// 員工編號: 25 19 27 共 3 人!」
-//		int ar[][] = { { 25, 2500 }, { 32, 800 }, { 8, 500 }, { 19, 1000 }, { 27, 1200 } };
-//		Scanner sc = new Scanner(System.in);
-//		System.out.println("請輸入要預借的金額");
-//		int total3 = 0;
-//		int an = sc.nextInt();
-//		System.out.println("有錢可借的員工為:");
-//		for (int i = 0; i < ar.length; i++) {
-//			for (int j = 1; j < ar[i].length; j++) {
-//				if (an <= ar[i][1]) {
-//
-//					System.out.print(ar[i][0] + "\t");
-//					total3++;
-//				}
-//			}
-//		}
-//
-//		System.out.println("共" + total3 + "人!");
-//
-//		System.out.println("=====================================================================");
-//		// • 請設計由鍵盤輸入三個整數,分別代表西元yyyy年,mm月,dd日,執行後會顯示是該年的第幾天
-//		// 例:輸入 1984 9 8 三個號碼後,程式會顯示「輸入的日期為該年第252天」
-//		int[] arr3 = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-//		int x = 0; // 設立變數x是為了讓陣列可以依序相加
-//		int daytotal = 0;
-//		Scanner ss = new Scanner(System.in);
-//		System.out.println("請輸入西元yyyy年,mm月,dd日");
-//		int yy = ss.nextInt();
-//		int mm = ss.nextInt();
-//		int dd = ss.nextInt();
-//
-//		if (yy % 4 == 0 && yy % 100 != 0 || yy % 400 == 0) { // 判斷是否為閏年，是的話讓2月天數加一
-//			arr3[1]++;
-//		}
-//		for (int i = 1; i <= mm - 1; i++) {
-//			if (mm == 2 && dd > 29) {
-//				System.out.println("2月不應該超過29天!"); // 防止2月超過29天
-//				daytotal = 0;
-//				dd = 0;
-//			} else {
-//				daytotal += arr3[x]; // 進行天數計算x代表從索引0(第一個月)開始計算
-//				x++;
-//			}
-//		}
-//		System.out.println("輸入的日期為該年第" + (daytotal + dd) + "天");
+		int ar[][] = { { 25, 2500 }, { 32, 800 }, { 8, 500 }, { 19, 1000 }, { 27, 1200 } };
+		Scanner sc = new Scanner(System.in);
+		System.out.println("請輸入要預借的金額");
+		int total3 = 0;
+		int an = sc.nextInt();
+		System.out.println("有錢可借的員工為:");
+		for (int i = 0; i < ar.length; i++) {
+			for (int j = 1; j < ar[i].length; j++) {
+				if (an <= ar[i][1]) {
+
+					System.out.print(ar[i][0] + "\t");
+					total3++;
+				}
+			}
+		}
+
+		System.out.println("共" + total3 + "人!");
+
+		System.out.println("=====================================================================");
+		// • 請設計由鍵盤輸入三個整數,分別代表西元yyyy年,mm月,dd日,執行後會顯示是該年的第幾天
+		// 例:輸入 1984 9 8 三個號碼後,程式會顯示「輸入的日期為該年第252天」
+		int[] arr3 = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		int x = 0; // 設立變數x是為了讓陣列可以依序相加
+		int daytotal = 0;
+		Scanner ss = new Scanner(System.in);
+		System.out.println("請輸入西元yyyy年,mm月,dd日");
+		int yy = ss.nextInt();
+		int mm = ss.nextInt();
+		int dd = ss.nextInt();
+
+		if (yy % 4 == 0 && yy % 100 != 0 || yy % 400 == 0) { // 判斷是否為閏年，是的話讓2月天數加一
+			arr3[1]++;
+		}
+		for (int i = 1; i <= mm - 1; i++) {
+			if (mm == 2 && dd > 29) {
+				System.out.println("2月不應該超過29天!"); // 防止2月超過29天
+				daytotal = 0;
+				dd = 0;
+			} else {
+				daytotal += arr3[x]; // 進行天數計算x代表從索引0(第一個月)開始計算
+				x++;
+			}
+		}
+		System.out.println("輸入的日期為該年第" + (daytotal + dd) + "天");
+		
 			//		班上有8位同學,他們進行了6次考試結果如下:
 					int[][]ex= {
 					{10,35,40,100,90,85,75,70},
@@ -111,19 +112,19 @@ public class Hw4_1 {
 					{98,70,89,90,75,90,89,90},
 					{90,80,100,75,50,20,99,75},
 					};
-					int ex1[] = new int[8];
+					int ex1[] = new int[8];//宣告空一維陣列來當同學次數的計算
 					for (int i = 0; i < ex.length; i++) {
-						int max = ex[i][0];	
+						int max = ex[i][0];	//宣告max來當作第每列第一個數字來比出最大值
 						int p=0;
-						int b=0;
+						int last=0;
 						for (int j = 1; j < ex[i].length; j++) {
 							p++;
-							if (ex[i][j] > max) {
+							if (ex[i][j] > max) { //當後面的數字有比max大就給他賦值以此來找出最大值
 								max = ex[i][j];
-								b = j;
+								last = j;//這邊的last是來儲存最大max值得索引[j]以此來知道是哪位同學要加1							
 							} 
-						if(p==7) {
-							ex1[b]+=1;
+						if(p==7) {        //當全部數字跑完時，就會把last當作ex1陣列的索引值(學號)然後加1做計算，
+							ex1[last]+=1; //以此類推跑完6列
 						}
 						}
 						
@@ -135,7 +136,6 @@ public class Hw4_1 {
 					System.out.println("5號同學考最高分的次數="+ex1[4]);
 					System.out.println("6號同學考最高分的次數="+ex1[5]);
 					System.out.println("7號同學考最高分的次數="+ex1[6]);
-					System.out.println("8號同學考最高分的次數="+ex1[7]);
-					
+					System.out.println("8號同學考最高分的次數="+ex1[7]);					
 				}
 			}
